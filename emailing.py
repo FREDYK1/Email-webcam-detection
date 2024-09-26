3009ec7292fe5a1d3cc0086363ddb5915f4e317d
FREDYK1 <fkwekukankam@gmail.com>
2024-09-26 13:44:17 +0000

    Create function to send email containing an attachment

--- emailing.py
+++ emailing.py
@@ -1,2 +1,29 @@
-def send_email():
-    print("Sent Email!")
+import smtplib
+import imghdr
+from email.message import EmailMessage
+
+
+PASSWORD = "vtse cxha iaxe odwh"
+SENDER = "frederickkankam7@gmail.com"
+RECEIVER = "fkwekukankam@gmail.com"
+
+
+def send_email(image):
+    email_message = EmailMessage()
+    email_message["Subject"] ="A New Customer Has Arrived"
+    email_message.set_content("A new customer has arrived. "
+                              "Please check the attached image.")
+
+    with open(image, "rb") as file:
+        content = file.read()
+
+    email_message.add_attachment(content, maintype="image",
+                                 subtype=imghdr.what(None, content))
+
+    gmail =smtplib.SMTP("smtp.gmail.com", 587)
+    gmail.ehlo()
+    gmail.starttls()
+    gmail.login(SENDER, PASSWORD)
+    gmail.send_message(SENDER, RECEIVER, email_message.as_string())
+    gmail.quit()
+
